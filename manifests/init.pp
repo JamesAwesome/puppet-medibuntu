8f6fcd3d0bdfd7ef9623560ccf54c407da3beeab
James Awesome <jamestheawesome@gmail.com>
2013-04-14 20:17:28 -0400

    moved includes into operatingsystem case

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,21 +5,21 @@ class medibuntu(
 
   # Distro has to be ubuntu.
   case $::operatingsystem {
-    'Ubuntu': { }
+    'Ubuntu': {
+       # If user wishes to install hooks for the
+       # Ubuntu software center, do so. This is
+       # done by default.
+       case $apport {
+         false:   { include medibuntu::repo
+         }
+         default: { include medibuntu::repo,
+           medibuntu::apport
+         }
+       }
+      }
     default:  {
       err('This module only supports the Ubuntu
         Gnu/Linux distribution')
-    }
-  }
-
-  # If user wishes to install hooks for the
-  # Ubuntu software center, do so. This is
-  # done by default.
-  case $apport {
-    false:   { include medibuntu::repo
-    }
-    default: { include medibuntu::repo,
-      medibuntu::apport
     }
   }
 
